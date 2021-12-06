f980e2a17ad026d111f3bdb108fb18b02f6fd877
tomstark <zhantong@sensetime.com>
2021-12-05 20:40:47 -0500

    [add] admin parallel get

--- purchase/views.py
+++ purchase/views.py
@@ -3,10 +3,7 @@ import json
 from ResponseUtil import Response
 
 def index(request):
-    if request.method == 'POST':
         resp = Response().success("Set token Successfully!")
-        return HttpResponse(json.dumps(resp), content_type="application/json")
-    elif request.method == "GET":
         return HttpResponse(json.dumps(resp), content_type="application/json")
 
 
