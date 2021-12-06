82f759e7a4885b9b7ee2df239334bb324c121c7c
cheng09 <czhang43@buffaloledu>
2021-12-05 21:35:51 -0500

    [add] purchase module

--- purchase/views.py
+++ purchase/views.py
@@ -1,14 +1,46 @@
 from django.http import HttpResponse
 import json
 from ResponseUtil import Response
+import requests
+from ConstantUtil import Constant
 
 def index(request):
+    userPath = 'http://3.95.6.253:8000/infos/users'
+    productPath = 'http://ccsprintproduct.us-east-1.elasticbeanstalk.com/infos/products'
+    orderPath = 'http://34.205.127.101:8000/infos/orders'
+
     if request.method == 'POST':
-        resp = Response().success("Set token Successfully!")
-        return HttpResponse(json.dumps(resp), content_type="application/json")
-    elif request.method == "GET":
-        return HttpResponse(json.dumps(resp), content_type="application/json")
+        requestDict = eval(request.body)
+        if requestDict:
+            # User info
+            userID = str(requestDict.get('userID'))
+            user = requests.get(userPath+'/'+userID)
+            # Product info
+            productID = str(requestDict.get('productID'))
+            product = requests.get(productPath+'/'+productID)
 
+            # Check user and product if request fail, return not found
+            if user.status_code != 200 or product.status_code != 200:
+                resp = Response().failed()
+                return HttpResponse(json.dumps(resp), content_type="application/json", status=404)
 
+            # Check stock if it is less than demand, return bad data
+            stock = product.json()["data"]["amount"]
+            quantity = requestDict.get('quantity')
+            if stock < quantity:
+                resp = Response().resp(Constant().BAD_DATA, "Out of stock")
+                return HttpResponse(json.dumps(resp), content_type="application/json", status=400)
 
+            # Place order
+            order = requests.post(orderPath, data=json.dumps(requestDict))
 
+            # Update stock
+            productInfo = product.json()["data"]
+            productInfo["amount"] = productInfo["amount"] - quantity
+            del productInfo['product_id']
+            requests.put(productPath+'/'+productID, data=json.dumps(productInfo))
+
+            response = Response().resp(Constant().POST, order.json()['data'])
+            return HttpResponse(json.dumps(response), content_type="application/json", status=201)
+        resp = Response().failed()
+        return HttpResponse(json.dumps(resp), content_type="application/json", status=404)
